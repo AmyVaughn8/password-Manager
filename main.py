24ea5f0f9781231950413643b98b01ae5f9ec5cf
Amy Vaughn <127699301+AmyVaughn8@users.noreply.github.com>
2023-11-21 16:40:01 -0600

    Add labels and entry fields to GUI

--- main.py
+++ main.py
@@ -5,12 +5,30 @@ window = tk.Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = tk.Canvas(height=300, width=300)
-canvas.pack()
+# canvas
+canvas = tk.Canvas(height=300, width=200)
+canvas.grid(row=0, column=1)
 
+# image
 original_image = Image.open("lock-logo.png")
 resized_image = original_image.resize((150,150), Image.Resampling.LANCZOS)
 logo_img = ImageTk.PhotoImage(resized_image)
 canvas.create_image(150,150,image=logo_img)
+
+# labels
+website_label = tk.Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = tk.Label(text="Email / Username")
+email_label.grid(row=2, column=0)
+password_label = tk.Label(text="Password")
+password_label.grid(row=3, column=0)
+
+# entries
+website_entry = tk.Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = tk.Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = tk.Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 window.mainloop()
