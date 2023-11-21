8f24eb92d6dd546c04acce0590ec1cc88d114c72
Amy Vaughn <127699301+AmyVaughn8@users.noreply.github.com>
2023-11-21 13:21:11 -0600

    Resize logo image for main window display

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 window = tk.Tk()
-
 window.title("Password Manager")
-
 window.config(padx=20, pady=20)
 
 canvas = tk.Canvas(height=300, width=300)
+canvas.pack()
+
+original_image = Image.open("lock-logo.png")
+resized_image = original_image.resize((150,150), Image.Resampling.LANCZOS)
+logo_img = ImageTk.PhotoImage(resized_image)
+canvas.create_image(150,150,image=logo_img)
 
 window.mainloop()
